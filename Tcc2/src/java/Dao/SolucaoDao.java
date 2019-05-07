377b467f736ebbad06be0ba4958a460b7169b159
Yuri Lima <kimusseila@hotmail.com>
2019-05-06 21:42:35 -0300

    Autor

--- Tcc2/src/java/Dao/SolucaoDao.java
+++ Tcc2/src/java/Dao/SolucaoDao.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Dao;
 
 import Modelo.Solucao;
 
 /**
  *
- * @author eliane
+ * @author yuri
  */
 public class SolucaoDao extends GenericDAO<Solucao, Integer>{    
     public SolucaoDao(){
