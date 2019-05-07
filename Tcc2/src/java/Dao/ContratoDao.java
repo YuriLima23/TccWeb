b3b9d9e997ed090e8e8aa25856d5075cb7829abb
Yuri Lima <kimusseila@hotmail.com>
2019-05-06 21:38:30 -0300

    Autor

--- Tcc2/src/java/Dao/ContratoDao.java
+++ Tcc2/src/java/Dao/ContratoDao.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Dao;
 
 import Modelo.Contrato;
 
 /**
  *
- * @author eliane
+ * @author yuri
  */
 public class ContratoDao extends GenericDAO<Contrato, Integer>{    
     public ContratoDao(){
