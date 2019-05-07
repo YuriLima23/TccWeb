e285608445d839ad703635ce892b50bfd06e8f59
Yuri Lima <kimusseila@hotmail.com>
2019-05-06 21:36:57 -0300

    Autor

--- Tcc2/src/java/Dao/CategoriaDao.java
+++ Tcc2/src/java/Dao/CategoriaDao.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Dao;
 
 import Modelo.Categoria;
@@ -10,7 +6,7 @@ import java.util.List;
 
 /**
  *
- * @author eliane
+ * @author yuri
  */
 
 public class CategoriaDao extends GenericDAO<Categoria, Integer>{    
