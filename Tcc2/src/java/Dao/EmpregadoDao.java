f404e7d14a96e189691755259ace9573572a9ea0
Yuri Lima <kimusseila@hotmail.com>
2019-05-06 21:39:21 -0300

    Autir

--- Tcc2/src/java/Dao/EmpregadoDao.java
+++ Tcc2/src/java/Dao/EmpregadoDao.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Dao;
 
 import Modelo.Categoria;
@@ -13,7 +9,7 @@ import javax.persistence.Query;
 
 /**
  *
- * @author eliane
+ * @author yuri
  */
 public class EmpregadoDao extends GenericDAO<Empregado, Integer> {
 
